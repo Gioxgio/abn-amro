33bcf19887d818c4ab3add92f7f6292ed1c41eee
Giorgio Gagliardi <giorgio.gagliardi@klarna.com>
2023-09-21 07:28:45 +0200

    Removed test

--- src/test/java/com/abnamro/abnamro/AbnAmroApplicationTests.java
+++ src/test/java/com/abnamro/abnamro/AbnAmroApplicationTests.java
@@ -1,13 +1,7 @@
 package com.abnamro.abnamro;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class AbnAmroApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
 }
